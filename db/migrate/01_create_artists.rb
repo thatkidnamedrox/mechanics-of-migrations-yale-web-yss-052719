b0b07a5f9aee58829949d9667b69d0252c9cd77e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-08 04:15:00 +0000

    Automatically backed up by Learn

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,16 @@
+class CreateArtists < AcitveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
